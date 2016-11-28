7201a92735d955cf67e0a0502ebd5f17f8dc75f8
Hal Fairless <hbfairless@gmail.com>
2016-11-28 15:38:43 -0800

    added scalene, isosceles and equilateral rspec test and corresponding method logic

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,27 +1,34 @@
-require 'pry'
-
 class Triangle
 
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-  end
 
   def triangle
     triangle_type = ""
-    if @side_1 == 0 or @side_2 == 0 or @side_3 == 0
-      triangle_type = "zero length not allowed"
-    elsif @side_1 == @side_2 && @side_1 == @side_3
-          triangle_type = "equilateral triangle" 
-    end
-    # individual_words = phrase.split()
-    #
-    # individual_words.each do |individual_word|
-    #   if individual_word.downcase == word.downcase
-    #      count += 1
-    #   end
-    # end
+
+  if @side_1 == 0 or @side_2 == 0 or @side_3 == 0
+    triangle_type = "zero length not allowed"
+
+  elsif ((@side_1 + @side_2) <= @side_3) or ((@side_1 + @side_3) <= @side_2) or ((@side_2 + @side_3) <= @side_1)
+      triangle_type = "not a triangle"
+
+  elsif ((@side_1 != @side_2) and (@side_2 != @side_3) and (@side_1 != @side_3))
+    triangle_type = "scalene triangle"
+
+
+  elsif (((@side_1 == @side_2) && (@side_1 != @side_3)) or ((@side_2 == @side_3) && (@side_2 != @side_1)) or ((@side_1 = @side_3) && (@side_1 != @side_2)))
+    triangle_type = "isosceles triangle"
+
+  elsif @side_1 == @side_2 && @side_1 == @side_3
+      triangle_type = "equilateral triangle"
+    else
+      triangle_type = "major problem"
+  end
     triangle_type
+
+  end
+
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -5,31 +5,29 @@ require('./lib/triangle')
   describe (Triangle) do
     describe '#triangle' do
 
-    it 'identifies the type of triangle' do
+    it 'identifies a zero value entered' do
       triangle_type = Triangle.new(0, 0, 0)
-      expect(triangle_type.triangle).to eq("zero length not allowed")
+      expect(triangle_type.triangle).to(eq("zero length not allowed"))
     end
 
+    it 'identifies a non-triangle' do
+      triangle_type = Triangle.new(20, 10, 10)
+      expect(triangle_type.triangle).to(eq("not a triangle"))
+    end
 
+    it 'identifies a scalene triangle' do
+      triangle_type = Triangle.new(20, 10, 15)
+      expect(triangle_type.triangle).to(eq("scalene triangle"))
+    end
 
-    it 'identifies the type of triangle' do
+    it 'identifies an isosceles triangle' do
+      triangle_type = Triangle.new(25, 15, 15)
+    expect(triangle_type.triangle).to(eq("isosceles triangle"))
+    end
+
+    it 'identifies an equilateral triangle' do
       triangle_type = Triangle.new(10, 10, 10)
-    expect(triangle_type.triangle).to eq("equilateral triangle")
+    expect(triangle_type.triangle).to(eq("equilateral triangle"))
     end
   end
-    # it 'identifies the type of triangle' do
-    #   triangle = Triangle.new((0, 0, 0))
-    #   expect(triangle.count).to eq 0
-    # end
-    #
-    # it 'identifies the type of triangle' do
-    #   triangle = Triangle.new((0, 0, 0))
-    #   expect(triangle.count).to eq 1
-    # end
-    #
-    # it 'identifies the type of triangle' do
-    #   triangle = Triangle.new((0, 0, 0))
-    #   expect(triangle.count).to eq 1
-    # end
-    # end
 end
