bdc09bc5e1c9f3e209cd11510d4a37ce0e84653d
Hal Fairless <hbfairless@gmail.com>
2016-11-28 13:12:58 -0800

    adds a new rspec test

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,23 +2,26 @@ require 'pry'
 
 class Triangle
 
-  attr_reader :side_1, :side_2, :side_3
-
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
   end
 
-  # def count
-  #   count = 0
-  #   individual_words = phrase.split()
-  #
-  #   individual_words.each do |individual_word|
-  #     if individual_word.downcase == word.downcase
-  #        count += 1
-  #     end
-  #   end
-  #   count
-  # end
+  def triangle
+    triangle_type = ""
+    if @side_1 == 0 or @side_2 == 0 or @side_3 == 0
+      triangle_type = "zero length not allowed"
+    elsif @side_1 == @side_2 && @side_1 == @side_3
+          triangle_type = "equilateral triangle" 
+    end
+    # individual_words = phrase.split()
+    #
+    # individual_words.each do |individual_word|
+    #   if individual_word.downcase == word.downcase
+    #      count += 1
+    #   end
+    # end
+    triangle_type
+  end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,18 +2,21 @@
 require('rspec')
 require('./lib/triangle')
 
-  describe '#triangle_type' do
+  describe (Triangle) do
+    describe '#triangle' do
 
     it 'identifies the type of triangle' do
-      triangle = Triangle.new(0, 0, 0)
-      expect(triangle.count).to eq 1
+      triangle_type = Triangle.new(0, 0, 0)
+      expect(triangle_type.triangle).to eq("zero length not allowed")
     end
 
-    # it 'identifies the type of triangle' do
-    #   triangle = Triangle.new((0, 0, 0))
-    #   expect(triangle.count).to eq 2
-    # end
-    #
+
+
+    it 'identifies the type of triangle' do
+      triangle_type = Triangle.new(10, 10, 10)
+    expect(triangle_type.triangle).to eq("equilateral triangle")
+    end
+  end
     # it 'identifies the type of triangle' do
     #   triangle = Triangle.new((0, 0, 0))
     #   expect(triangle.count).to eq 0
@@ -28,4 +31,5 @@ require('./lib/triangle')
     #   triangle = Triangle.new((0, 0, 0))
     #   expect(triangle.count).to eq 1
     # end
-  end
+    # end
+end
